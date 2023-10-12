271d052b8ecbd14fe124c18f3bec837eac0514de
miriyala srikanth <123526232+srikanthmiriyala017@users.noreply.github.com>
2023-10-12 21:00:07 +0530

    Create Piegraph.js

---
+++ src/components/Graph/Piegraph/Piegraph.js
@@ -0,0 +1,10 @@
+import './Piegraph.css'
+export default function PieGraph(){
+    return (
+        <div className="piegraphMainDiv">
+            <div className="pieHeading">Customers</div>
+            <div className="piesubheading">Customers that buy products</div>
+            <div className="piechart"></div>
+        </div>
+    )
+}
